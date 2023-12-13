7d2cdbee67c753e40884f64b5e288c7aafdafeea
Mor Marzan <mormarzan@gmail.com>
2023-12-13 17:24:01 +0200

    Add unread mail style

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,10 +1,17 @@
 import { utilService } from "../../../services/util.service.js"
+
+
 export function MailPreview({ mail }) {
+    const { subject, body, isRead, sentAt, from} = mail
+
+    const readStat = !isRead ? 'unread' : ''
+
     return (
-        <article className="mail-preview">
-            <div className="from text-overflow-ellipsis">{mail.from}</div>
-            <div className="mail-body text-overflow-ellipsis"><span className="subject">{mail.subject}</span> - {mail.body}</div>
-            <div className="date">{utilService.formatTimestamp(mail.sentAt)}</div>
+        // <article className="mail-preview">
+        <article className={readStat + ' mail-preview'}>
+            <div className="from text-overflow-ellipsis">{from}</div>
+            <div className="mail-content text-overflow-ellipsis"><span className="subject">{subject}</span> - {body}</div>
+            <div className="date">{utilService.formatTimestamp(sentAt)}</div>
         </article>
     )
 }
