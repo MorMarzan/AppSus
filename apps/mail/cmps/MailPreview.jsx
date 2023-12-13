475531af9eac391c91ed97c29a723ca32efc1738
Mor Marzan <mormarzan@gmail.com>
2023-12-13 15:57:32 +0200

    Delivery1 - Wednesday 16:00

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -3,7 +3,7 @@ export function MailPreview({ mail }) {
     return (
         <article className="mail-preview">
             <div className="from text-overflow-ellipsis">{mail.from}</div>
-            <div className="mail-body text-overflow-ellipsis">{mail.subject} - {mail.body}</div>
+            <div className="mail-body text-overflow-ellipsis"><span className="subject">{mail.subject}</span> - {mail.body}</div>
             <div className="date">{utilService.formatTimestamp(mail.sentAt)}</div>
         </article>
     )
