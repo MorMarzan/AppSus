f758f264ea2b484505fda7e61a2d3fb93740f64b
Mor Marzan <mormarzan@gmail.com>
2023-12-15 17:32:59 +0200

    Add back btn from details to index

--- apps/mail/cmps/MailHeader.jsx
+++ apps/mail/cmps/MailHeader.jsx
@@ -1,11 +1,24 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink, useLocation, useParams } = ReactRouterDOM
 
-export function MailHeader({onSetIsSbFull}) {
+export function MailHeader() {
+
+    const location = useLocation()
+    const { mailId } = useParams()
+
+    // const isMailInboxRoute = location.pathname === '/mail/inbox/';
+    const hasMailId = !!mailId;
+    // console.log('hasMailId', hasMailId)
+    // console.log('isMailInboxRoute', isMailInboxRoute)
+
     return (
         <header className="mail-header full main-layout">
             <section>
-                {/* <button onClick={onSetIsSbFull}><i className="fa-solid fa-bars"></i></button> */}
-                <h1>Mail Header</h1>
+                {hasMailId &&
+                    <Link className="btn"
+                        to={'/mail/inbox'}>
+                        <i className="fa-solid fa-arrow-left"></i>
+                    </Link>
+                }
                 {/* <nav className="app-nav">
                     <NavLink to="/" >Home</NavLink>
                     <NavLink to="/about" >About</NavLink>
