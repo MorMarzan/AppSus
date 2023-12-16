7cc5c523e7d36faf560e799e452817a1a50e3504
EdenRize <edenrize@gmail.com>
2023-12-16 22:19:32 +0200

    fix youtube videos

--- apps/note/cmps/NoteVideo.jsx
+++ apps/note/cmps/NoteVideo.jsx
@@ -6,7 +6,11 @@ export function NoteVideo({ note }) {
     >
       {note.info.title && <p className="note-title">{note.info.title}</p>}
       <h2 className="note-content">
-        <iframe className="video" height="235" src={note.info.url}></iframe>
+        <iframe
+          className="video"
+          height="235"
+          src={note.info.url.replace('watch?v=', 'embed/')}
+        ></iframe>
       </h2>
     </article>
   )
