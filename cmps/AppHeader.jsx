455867ced4a2a29812919610eadc1841fafe3747
EdenRize <edenrize@gmail.com>
2023-12-16 19:25:37 +0200

    Add nav bar to mobile

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -9,13 +9,51 @@ export function AppHeader() {
           <h3>AppSus</h3>
         </div>
       </Link>
+
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/mail">Mail</NavLink>
-        <NavLink to="/note">Note</NavLink>
-        <NavLink to="/book">Miss Books</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <div id="menuToggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <ul id="menu">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/mail">Mail</NavLink>
+            </li>
+            <li>
+              <NavLink to="/note">Note</NavLink>
+            </li>
+            <li>
+              <NavLink to="/book">Miss Books</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   )
+}
+
+{
+  /* // <header className="app-header">
+    //   <Link to="/">
+    //     <div className="logo-container">
+    //       <img className="app-logo" src="./assets/img/appsus-logo.png" />
+    //       <h3>AppSus</h3>
+    //     </div>
+    //   </Link>
+    //   <nav>
+    //     <NavLink to="/">Home</NavLink>
+    //     <NavLink to="/mail">Mail</NavLink>
+    //     <NavLink to="/note">Note</NavLink>
+    //     <NavLink to="/book">Miss Books</NavLink>
+    //     <NavLink to="/about">About</NavLink>
+    //   </nav>
+    // </header> */
 }
