eb38054e1b3394359d6cb330014b4d14a70a33e2
EdenRize <edenrize@gmail.com>
2023-12-16 15:34:18 +0200

    Add close nav on click for mobile and fix compose min width

--- apps/mail/cmps/MailSidebar.jsx
+++ apps/mail/cmps/MailSidebar.jsx
@@ -3,13 +3,22 @@ import { DynamicLogo } from '../../../cmps/DynamicLogo.jsx'
 const { Link, NavLink, useNavigate } = ReactRouterDOM
 const { useState, useEffect } = React
 
-export function MailSidebar({ isMobileSbOpen, isDesktopSbOpen, isMobile }) {
+export function MailSidebar({
+  isMobileSbOpen,
+  isDesktopSbOpen,
+  isMobile,
+  onSetIsSbOpen,
+}) {
   const navigate = useNavigate()
 
   function openNewCompose() {
     navigate({
       search: `?compose=new`,
     })
+  }
+
+  function onNav() {
+    if (isMobile) onSetIsSbOpen()
   }
 
   const dynClass = isMobileSbOpen ? 'open' : ''
@@ -19,29 +28,35 @@ export function MailSidebar({ isMobileSbOpen, isDesktopSbOpen, isMobile }) {
       {isMobile && <DynamicLogo page={'mail'} />}
 
       <div className="sidebar-btns">
-        <button onClick={openNewCompose} className="btn">
+        <button
+          onClick={() => {
+            openNewCompose()
+            onNav()
+          }}
+          className="btn"
+        >
           <i className="fa-solid fa-pencil"></i>
           {isDesktopSbOpen && <span>Compose</span>}
         </button>
 
         <nav className="app-nav">
-          <NavLink className="btn" to="/mail/inbox">
+          <NavLink onClick={onNav} className="btn" to="/mail/inbox">
             <i className="fa-solid fa-inbox"></i>
             {isDesktopSbOpen && <span>Inbox</span>}
           </NavLink>
-          <NavLink className="btn" to="/mail/starred">
+          <NavLink onClick={onNav} className="btn" to="/mail/starred">
             <i className="fa-regular fa-star"></i>
             {isDesktopSbOpen && <span>Starred</span>}
           </NavLink>
-          <NavLink className="btn" to="/mail/sent">
+          <NavLink onClick={onNav} className="btn" to="/mail/sent">
             <i className="fa-regular fa-paper-plane"></i>
             {isDesktopSbOpen && <span>Sent</span>}
           </NavLink>
-          <NavLink className="btn" to="/mail/draft">
+          <NavLink onClick={onNav} className="btn" to="/mail/draft">
             <i className="fa-regular fa-pen-to-square"></i>
             {isDesktopSbOpen && <span>Draft</span>}
           </NavLink>
-          <NavLink className="btn" to="/mail/bin">
+          <NavLink onClick={onNav} className="btn" to="/mail/bin">
             <i className="fa-solid fa-trash"></i>
             {isDesktopSbOpen && <span>Bin</span>}
           </NavLink>
--- apps/note/cmps/NoteSidebar.jsx
+++ apps/note/cmps/NoteSidebar.jsx
@@ -2,7 +2,16 @@ import { DynamicLogo } from '../../../cmps/DynamicLogo.jsx'
 
 const { Link, NavLink } = ReactRouterDOM
 
-export function NoteSidebar({ isMobileSbOpen, isDesktopSbOpen, isMobile }) {
+export function NoteSidebar({
+  isMobileSbOpen,
+  isDesktopSbOpen,
+  isMobile,
+  onSetIsSbOpen,
+}) {
+  function onNav() {
+    if (isMobile) onSetIsSbOpen()
+  }
+
   return (
     <section
       className={`sidebar note-sidebar ${isMobileSbOpen && 'open'} ${
@@ -12,11 +21,11 @@ export function NoteSidebar({ isMobileSbOpen, isDesktopSbOpen, isMobile }) {
       {isMobile && <DynamicLogo page={'keep'} />}
 
       <div className="sidebar-btns">
-        <NavLink className="btn" to="/note/notes">
+        <NavLink onClick={onNav} className="btn" to="/note/notes">
           <img src="./assets/img/bulb.svg" />
           {isDesktopSbOpen && <span>Notes</span>}
         </NavLink>
-        <NavLink className="btn" to="/note/bin">
+        <NavLink onClick={onNav} className="btn" to="/note/bin">
           <img src="./assets/img/trash.svg" />
           {isDesktopSbOpen && <span>Bin</span>}
         </NavLink>
