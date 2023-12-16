3bd2762edc06e1e49626ac78f8ff50caea98260c
EdenRize <edenrize@gmail.com>
2023-12-16 15:41:48 +0200

    remove logo name in header in mobile

--- cmps/DynamicLogo.jsx
+++ cmps/DynamicLogo.jsx
@@ -1,8 +1,25 @@
+const { useState, useEffect } = React
+
 export function DynamicLogo({ page }) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  function handleResize() {
+    const currIsMobile = window.innerWidth <= 500
+    setIsMobile(currIsMobile)
+  }
+
   return (
     <div className="dynamic-logo">
       <img src={`./assets/img/${page}.svg`} />
-      <h2>{page === 'mail' ? 'Gmail' : 'Notes'}</h2>
+      {!isMobile && <h2>{page === 'mail' ? 'Gmail' : 'Notes'}</h2>}
     </div>
   )
 }
