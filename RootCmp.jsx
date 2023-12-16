c551e6d6bb15103203506cb2be8b04f706171071
EdenRize <edenrize@gmail.com>
2023-12-16 04:08:41 +0200

    Some styling

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -19,26 +19,6 @@ const { useState, useEffect } = React
 import { eventBusService } from '../../../services/event-bus.service.js'
 
 export function App() {
-  const [isShowLoader, setIsShowLoader] = useState(false)
-
-  useEffect(() => {
-    const unsubscribeShowLoader = eventBusService.on('show-loader', () => {
-      console.log('show')
-      setIsShowLoader(true)
-    })
-    const unsubscribeHideLoader = eventBusService.on('hide-loader', () => {
-      console.log('show')
-      setIsShowLoader(false)
-    })
-
-    console.log('starting')
-
-    return () => {
-      unsubscribeShowLoader()
-      unsubscribeHideLoader()
-    }
-  }, [])
-
   return (
     <Router>
       <section className="app">
